503b1357e0398fa15e001cfc8a57575572a54606
catrinehom <48596773+catrinehom@users.noreply.github.com>
2019-12-20 12:36:33 +0100

    Error Handling included

--- ReadReplacer.py
+++ ReadReplacer.py
@@ -9,11 +9,46 @@ Created on Thu Dec 12 10:36:28 2019
 
 # Import libraries 
 import sys
+import time
 from argparse import ArgumentParser
 
 ###########################################################################
 # FUNCTIONS
 ###########################################################################
+
+def CheckGZip(filename):
+    '''
+    This function checks if the input file is gzipped.
+    '''
+    gzipped_type = b'\x1f\x8b'
+    
+    infile = open(filename,'rb')
+    filetype = infile.read(2)
+    infile.close()
+    if filetype == gzipped_type:
+        return True
+    else:
+        return False
+    
+def CheckFastq(filenames):
+    """
+    This function checks if a input file is in fastq format.
+    Outputs True/False for the file.
+    """
+    fastq_type = "@"
+
+    # Open all files and get the first character
+    for infile in filenames:
+        f = OpenFile(infile, "r")
+        first_char = f.read(1);
+        f.close()
+        # Check if fastq
+        if first_char == fastq_type:
+            fastq = True
+        else:
+            fastq = False
+    return fastq
+
 
 def OpenFile(filename,mode):
     '''
@@ -22,8 +57,8 @@ def OpenFile(filename,mode):
     try:
         infile = open(filename,mode)   
     except IOError as error:
-        print('Could not open '+filename+' due to: '+str(error))
-        sys.exit(1)
+        message = 'Could not open '+filename+' due to: '+str(error)
+        sys.exit(message)
     return infile
 
 ###########################################################################
@@ -32,16 +67,68 @@ def OpenFile(filename,mode):
 
 # Input from command line
 parser = ArgumentParser()
-parser.add_argument('-a', dest='all_fastq',help='Input file to find IDs from')
-parser.add_argument('-b', dest='high_quality_fastq', help='Output filename')
+parser.add_argument('-a', dest='all_fastq',help='File with all fastq reads.')
+parser.add_argument('-q', dest='high_quality_fastq', help='File with fastq reads to replace in all fastq file.')
+parser.add_argument('-o', dest='outfilename', help='Output filename.')
 args = parser.parse_args()
 
-# Define input as variables
-all_fastq_filename = args.all_fastq
-high_quality_fastq_filename = args.high_quality_fastq
+###########################################################################
+# ERROR HANDLING
+###########################################################################
 
-all_fastq_filename = './test.fastq'
-high_quality_fastq_filename = './test_HAC.fastq'
+# Define input as variables
+if args.all_fastq is None:
+    all_fastq_filename = args.all_fastq
+else:
+    message = '-a option is missing. Please input a file with all fastq reads.'
+    sys.exit(message)
+    
+if args.high_quality_fastq is None:
+    high_quality_fastq_filename = args.high_quality_fastq
+else:
+    message = '-q option is missing. Please input a file with fastq reads to replace in all fastq file.'
+    sys.exit(message)
+    
+if args.outfilename is None:
+    outfilename = args.outfilename
+else:
+    message = '-o option is missing. Please input a output filename.'
+    sys.exit(message)
+    
+
+# Check if input files are in zipped
+a_check_zipped = CheckGZip(all_fastq_filename)
+h_check_zipped = CheckGZip(high_quality_fastq_filename)
+
+# Exit if the input files are not in fastq format
+if a_check_zipped is True:
+    message = "Input Error: {} is zipped. Unzip file to run pipeline.".format(all_fastq_filename)
+    sys.exit(message)
+
+if h_check_zipped is True:
+    message = "Input Error: {} is zipped. Unzip file to run pipeline.".format(high_quality_fastq_filename)
+    sys.exit(message)
+
+# Check if input files are in fastq format
+a_check_fastq = CheckFastq(all_fastq_filename)
+h_check_fastq = CheckFastq(high_quality_fastq_filename)
+
+
+# Exit if the input files are not in fastq format
+if a_check_fastq is False:
+    message = "Input Error: {} is a wrong format. Should be fasta format.".format(all_fastq_filename)
+    sys.exit(message)
+
+if h_check_fastq is False:
+    message = "Input Error: {} is a wrong format. Should be fasta format.".format(high_quality_fastq_filename)
+    sys.exit(message)
+
+# Start runtimer for program
+runtime_start = time.time()
+
+###########################################################################
+# FIND REPLACEMENT NAMES
+###########################################################################
 
 # Open files
 all_fastq_filename = OpenFile(all_fastq_filename,'r')
@@ -56,8 +143,12 @@ for line in high_quality_fastq:
         
 high_quality_fastq.close()   
 
+
+###########################################################################
+# SAVE TO NEW FILE
+###########################################################################
+
 # Open output file for mot matching reads:    
-outfilename = 'final.fastq'
 flag = False
 
 try:
@@ -74,8 +165,7 @@ for line in all_fastq_filename:
             flag = True
     if flag == True:
         print(line,file=outfile, end = '')
-        
-#.decode('ascii')
+
 
 high_quality_fastq = OpenFile(high_quality_fastq_filename,'r')
         
@@ -83,3 +173,10 @@ for line in high_quality_fastq:
     print(line,file=outfile, end = '')
 
 outfile.close()
+
+
+print(outfilename+' is saved with replaced reads.')
+
+# End runtimer for program and print
+runtime_end = time.time()
+print('Runtime: {0:.0f} seconds.'.format(runtime_end-runtime_start))
